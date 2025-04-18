1e43eb7337e84921c49322a848fd381b411c3dc5
scoubidix <val50.jean@hotmail.fr>
2025-04-18 14:57:39 +0200

    Réorganisation du frontend : suppression node_modules racine, dépendances dans frontend

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Dashboard from './Dashboard';
-console.log("Rendering Dashboard component");
-
-function App() {
-  return <Dashboard />;
-}
-
-export default App;
