1ee9d1c04721b8604c20e932ce0ce399a4ae1f63
Scoubidix <val50.jean@hotmail.fr>
2025-04-25 15:11:37 +0200

    Réorg + création dash kiné

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import DashboardPatient from './pages/patient/DashboardPatient';
+import DashboardKine from './pages/kine/DashboardKine';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/patient" element={<DashboardPatient />} />
+        <Route path="/kine" element={<DashboardKine />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/kine/DashboardKine.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DashboardKine() {
+  return (
+    <div>
+      <h1>Espace kiné</h1>
+      <p>Bienvenue dans votre tableau de bord professionnel.</p>
+    </div>
+  );
+}
